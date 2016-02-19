3ad3a519308ef5374b58288f4ee6977274af86d0
anurag regmi <anurag1234565@gmail.com>
2016-02-19 08:15:03 +0545

    added all files

---
+++ findWiki/MainWindow.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Gtk;
+using System.Diagnostics;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+
+	protected void onQuit (object sender, EventArgs e)
+	{
+		Application.Quit ();
+	}
+
+	protected void onHelp (object sender, EventArgs e)
+	{
+		result.Text = " Type the search in the text box and\n" +
+			" hit enter or click on Search button to search.";
+	}
+
+
+
+	protected void onSearch (object sender, EventArgs e)
+	{
+	
+		String text = entry1.Text;
+		Console.WriteLine (run_python (text));
+		result.Text = run_python(text);;
+	}
+
+     
+    protected string run_python(string args)
+	{
+		/*the code executes as " Filename Arguments " in shell
+
+		Here,
+		Filename = Python
+		Arguments = complete_address_of_python_file.py arguments 
+
+        i.e. it runs "python filename.py arguments " in the shell
+
+		StandardOutput stores the output of program displayed in the shell */ 
+
+		string directory = System.AppDomain.CurrentDomain.BaseDirectory;//for current directory of app
+
+		Process proc = new Process ();
+		proc.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+		proc.StartInfo.FileName = "python";
+		proc.StartInfo.Arguments = string.Format("{0}/wikisearch.py {1}", directory, args);
+		proc.StartInfo.UseShellExecute = false;
+		proc.StartInfo.RedirectStandardError = true;
+		proc.StartInfo.RedirectStandardInput = true;
+		proc.StartInfo.RedirectStandardOutput = true;
+		proc.Start ();
+
+		string output = (proc.StandardOutput.ReadToEnd());
+
+		return output;
+	}
+
+	protected void onEntered (object sender, EventArgs e)
+	{
+		onSearch(sender, e);
+	}
+}
---
+++ findWiki/bin/Debug/wikisearch.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+from sys import *
+from urllib2 import *
+from json import *
+
+
+def find(title):
+    # replaces space with %20(character used for space in url)
+    title = title.replace(" ", "%20")
+
+    # adding title to request
+    url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + title + "&redirects="
+
+    try:
+        # the required data is in {query:{pages{pageid{extract}}}}
+        data = urlopen(url)
+        jobject = load(data)
+        query = jobject['query']
+        pages = query['pages']
+        for i in pages:
+            abc = pages[i]
+        topic = abc['title']
+        summary = abc['extract']
+        summary = seperate(summary)
+        result = topic + summary
+
+    except:
+        result = "Search did not matched OR could not connect"
+
+    return result
+
+def seperate(string):
+    array = string.split()
+
+    for i in range(0, len(array),8):
+        array[i] = "\n" + array[i]
+
+    string = " ".join(array)
+    return string
+
+if __name__ == "__main__":
+    if len(argv)>1:
+        search = ""
+        for i in range(1, len(argv)):
+            search = search + " " + argv[i]
+        print find(search)
+    else :
+        print ("Type any thing to Search")
+
+
+
